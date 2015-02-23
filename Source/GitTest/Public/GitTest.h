3cd6241e17ba8e5aeed0b4ca02b0195994a587c8
Ioan Troana <ioan@eduroam-242-199.nomadic.bris.ac.uk>
2015-02-23 16:29:10 +0000

    initial commit

---
+++ Source/GitTest/Public/GitTest.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __GITTEST_H__
+#define __GITTEST_H__
+
+#include "EngineMinimal.h"
+
+#endif
---
+++ Source/GitTest/Public/GitTestGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "GitTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGitTestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AGitTestGameMode(const FObjectInitializer& ObjectInitializer);
+};
+
+
+
